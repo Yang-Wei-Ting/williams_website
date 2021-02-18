ac8fc2deec952948edcc3700d555cac7e56abf0d
Wei-Ting Yang <williamatcg@gmail.com>
2021-02-18 16:23:52 +0800

    commented urls

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
+from django.views.generic import RedirectView
 
 from .views import ProductList, ProductDetail
 
 
 urlpatterns = [
+    path('',                  RedirectView.as_view(url='products', permanent=True)),
     path('products',          ProductList.as_view()),
     path('products/<int:pk>', ProductDetail.as_view()),
 ]
--- config/urls.py
+++ config/urls.py
@@ -6,12 +6,18 @@ from home.views import home_view
 
 
 urlpatterns = [
+    # Home
+    path('',                 home_view, name='home'),
+
+    # Shop
+    path('shop/',            include('shop.urls')),
+
+    # API
+    path('api/v1/',          include('api.urls')),
+
+    # Admin and Auth
     path('admin/',           admin.site.urls),
     path('accounts/',        include('django.contrib.auth.urls')),
     path('accounts/signup/', signup_view, name='signup'),
-
-    path('',                 home_view,   name='home'),
-    path('shop/',            include('shop.urls')),
-    path('api/v1/',          include('api.urls')),
     path('api-auth/',        include('rest_framework.urls')),
 ]
