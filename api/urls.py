428e0b055f934ac35a9b467e6df26fc450981e25
Wei-Ting Yang <williamatcg@gmail.com>
2021-02-18 16:42:57 +0800

    added API link to header

--- api/urls.py
+++ api/urls.py
@@ -6,6 +6,6 @@ from .views import ProductList, ProductDetail
 
 urlpatterns = [
     path('',                  RedirectView.as_view(url='products', permanent=True)),
-    path('products',          ProductList.as_view()),
+    path('products',          ProductList.as_view(), name='product_list_api'),
     path('products/<int:pk>', ProductDetail.as_view()),
 ]
