645e6715340e17692306bc1b5a497b2895c5f36b
Wei-Ting Yang <williamatcg@gmail.com>
2021-02-18 17:31:51 +0800

    added view-level permissions for api

--- api/serializers.py
+++ api/serializers.py
@@ -6,4 +6,4 @@ from shop.models import Product
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = ('prod_name', 'prod_desc', 'prod_price', 'prod_imgname', 'prod_imgsrc', 'prodcat_id', 'vend_id')
+        fields = ('id', 'prod_name', 'prod_desc', 'prod_price', 'prod_imgname', 'prod_imgsrc', 'prodcat_id', 'vend_id')
--- api/views.py
+++ api/views.py
@@ -1,14 +1,17 @@
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
 from shop.models import Product
+from .permissions import IsSuperuserOrReadOnly
 from .serializers import ProductSerializer
 
 
 class ProductList(ListCreateAPIView):
+    permission_classes = (IsSuperuserOrReadOnly,)
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
 
 class ProductDetail(RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsSuperuserOrReadOnly,)
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
