63f5c4f00d07ca29da6ae64c7c30083466eba9f0
Wei-Ting Yang <williamatcg@gmail.com>
2021-02-19 20:45:56 +0800

    added app-level URLConfs in home and accounts apps

--- config/urls.py
+++ config/urls.py
@@ -1,23 +1,20 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from accounts.views import signup_view
-from home.views import home_view
-
 
 urlpatterns = [
     # Home
-    path('',                 home_view, name='home'),
+    path('',          include('home.urls')),
 
     # Shop
-    path('shop/',            include('shop.urls')),
+    path('shop/',     include('shop.urls')),
 
     # API
-    path('api/v1/',          include('api.urls')),
+    path('api/v1/',   include('api.urls')),
 
     # Admin and Auth
-    path('admin/',           admin.site.urls),
-    path('accounts/',        include('django.contrib.auth.urls')),
-    path('accounts/signup/', signup_view, name='signup'),
-    path('api-auth/',        include('rest_framework.urls')),
+    path('admin/',    admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('accounts.urls')),
+    path('api-auth/', include('rest_framework.urls')),
 ]
