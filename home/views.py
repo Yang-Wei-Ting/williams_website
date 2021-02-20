dc6b4759a9caa64caefb5a836424c876b99b14fc
Wei-Ting Yang <williamatcg@gmail.com>
2021-02-20 19:02:20 +0800

    followed best practice - "avoid using locals()" in home_view

--- home/views.py
+++ home/views.py
@@ -5,9 +5,6 @@ from .models import HomeCaption
 
 
 def home_view(request):
-    music_caption = HomeCaption.objects.get(purpose='About the Music')
-    cover_caption = HomeCaption.objects.get(purpose='About the Cover')
-
     current_hour = datetime.datetime.now().timetuple().tm_hour
 
     if request.session.get("visited", False):
@@ -16,4 +13,13 @@ def home_view(request):
         greeting_msg = ""
         request.session["visited"] = True
 
-    return render(request, 'home/home.html', locals())
+    cover_caption = HomeCaption.objects.get(purpose='About the Cover')
+    music_caption = HomeCaption.objects.get(purpose='About the Music')
+
+    context = {
+        'current_hour': current_hour,
+        'greeting_msg': greeting_msg,
+        'cover_caption': cover_caption,
+        'music_caption': music_caption,
+    }
+    return render(request, 'home/home.html', context=context)
