5902e428d1346d91fa2962d4fbb63d9eae0fdd36
Wei-Ting Yang <williamatcg@gmail.com>
2021-02-23 15:16:51 +0800

    merged views and templates of product list pages

--- shop/urls.py
+++ shop/urls.py
@@ -1,17 +1,13 @@
 from django.urls import path
 from django.views.generic import RedirectView
 
-from .views import (
-    product_category_list_view,
-    product_vendor_list_view,
-    product_detail_view,
-)
+from .views import product_list_view, product_detail_view
 
 
 urlpatterns = [
     path('',          RedirectView.as_view(url='products/browse-by-category/', permanent=True)),
-    path('products/', RedirectView.as_view(url='browse-by-category/', permanent=True)),
-    path('products/browse-by-category/', product_category_list_view, name='product_category_list'),
-    path('products/browse-by-vendor/',   product_vendor_list_view,   name='product_vendor_list'),
-    path('products/<int:pk>/',           product_detail_view,        name='product_detail'),
+    path('products/', RedirectView.as_view(url='browse-by-category/',          permanent=True)),
+    path('products/browse-by-category/', product_list_view,   kwargs={'browse_by': 'Category'}, name='product_category_list'),
+    path('products/browse-by-vendor/',   product_list_view,   kwargs={'browse_by': 'Vendor'},   name='product_vendor_list'),
+    path('products/<int:pk>/',           product_detail_view,                                   name='product_detail'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -5,70 +5,57 @@ from .models import Product
 from .forms import OrderForm
 
 
-def product_category_list_view(request):
+def product_list_view(request, browse_by):
     current_time = datetime.datetime.now()
     current_hour = current_time.timetuple().tm_hour
 
-    '''
-    SELECT shop_product.*
-    FROM shop_product INNER JOIN shop_productcategory
-      ON shop_product.prodcat_id_id = shop_productcategory.id
-    WHERE shop_productcategory.prodcat_name = '?????';
-    '''
     prods = Product.objects.all()
-    books_movies_music__prods  = a = prods.filter(prodcat_id__prodcat_name="Books, Movies & Music")
-    business_industrial__prods = b = prods.filter(prodcat_id__prodcat_name="Business & Industrial")
-    collectibles_art__prods    = c = prods.filter(prodcat_id__prodcat_name="Collectibles & Art")
-    electronics__prods         = d = prods.filter(prodcat_id__prodcat_name="Electronics")
-    fashion__prods             = e = prods.filter(prodcat_id__prodcat_name="Fashion")
-    food__prods                = f = prods.filter(prodcat_id__prodcat_name="Food")
-    health_beauty__prods       = g = prods.filter(prodcat_id__prodcat_name="Health & Beauty")
-    home_garden__prods         = h = prods.filter(prodcat_id__prodcat_name="Home & Garden")
-    motors__prods              = i = prods.filter(prodcat_id__prodcat_name="Motors")
-    sportinggoods__prods       = j = prods.filter(prodcat_id__prodcat_name="Sporting Goods")
-    toys_hobbies__prods        = k = prods.filter(prodcat_id__prodcat_name="Toys & Hobbies")
-    others__prods              = l = prods.filter(prodcat_id__prodcat_name="Others")
-    prods_group = (a, b, c, d, e, f, g, h, i, j, k, l)
+    if browse_by == 'Category':
+        '''
+        SELECT shop_product.*
+        FROM shop_product INNER JOIN shop_productcategory
+          ON shop_product.prodcat_id_id = shop_productcategory.id
+        WHERE shop_productcategory.prodcat_name = '?????';
+        '''
+        books_movies_music__prods  = a = prods.filter(prodcat_id__prodcat_name="Books, Movies & Music")
+        business_industrial__prods = b = prods.filter(prodcat_id__prodcat_name="Business & Industrial")
+        collectibles_art__prods    = c = prods.filter(prodcat_id__prodcat_name="Collectibles & Art")
+        electronics__prods         = d = prods.filter(prodcat_id__prodcat_name="Electronics")
+        fashion__prods             = e = prods.filter(prodcat_id__prodcat_name="Fashion")
+        food__prods                = f = prods.filter(prodcat_id__prodcat_name="Food")
+        health_beauty__prods       = g = prods.filter(prodcat_id__prodcat_name="Health & Beauty")
+        home_garden__prods         = h = prods.filter(prodcat_id__prodcat_name="Home & Garden")
+        motors__prods              = i = prods.filter(prodcat_id__prodcat_name="Motors")
+        sportinggoods__prods       = j = prods.filter(prodcat_id__prodcat_name="Sporting Goods")
+        toys_hobbies__prods        = k = prods.filter(prodcat_id__prodcat_name="Toys & Hobbies")
+        others__prods              = l = prods.filter(prodcat_id__prodcat_name="Others")
+    elif browse_by == 'Vendor':
+        '''
+        SELECT shop_product.*
+        FROM shop_product INNER JOIN shop_vendor
+          ON shop_product.vend_id_id = shop_vendor.id
+        WHERE shop_vendor.vend_name = '?????';
+        '''
+        abibas__prods     = a = prods.filter(vend_id__vend_name="Abibas")
+        banana__prods     = b = prods.filter(vend_id__vend_name="Banana")
+        f4fashion__prods  = c = prods.filter(vend_id__vend_name="F 4 Fashion")
+        fink__prods       = d = prods.filter(vend_id__vend_name="Fink Manufacturing")
+        microhard__prods  = e = prods.filter(vend_id__vend_name="Microhard")
+        mike__prods       = f = prods.filter(vend_id__vend_name="Mike")
+        penguin__prods    = g = prods.filter(vend_id__vend_name="Penguin Inc")
+        programing__prods = h = prods.filter(vend_id__vend_name="Programing Press")
+        ryan__prods       = i = prods.filter(vend_id__vend_name="Ryan Industries")
+        toysrthem__prods  = j = prods.filter(vend_id__vend_name="Toys R Them")
+        wcdonalds__prods  = k = prods.filter(vend_id__vend_name="WcDonald's")
+        unknown__prods    = l = prods.filter(vend_id__vend_name="Unknown")
 
     context = {
+        'browse_by': browse_by,
         'current_time': current_time,
         'current_hour': current_hour,
-        'prods_group': prods_group,
+        'prods_group': (a, b, c, d, e, f, g, h, i, j, k, l),
     }
-    return render(request, 'shop/product_category_list.html', context=context)
-
-
-def product_vendor_list_view(request):
-    current_time = datetime.datetime.now()
-    current_hour = current_time.timetuple().tm_hour
-
-    '''
-    SELECT shop_product.*
-    FROM shop_product INNER JOIN shop_vendor
-      ON shop_product.vend_id_id = shop_vendor.id
-    WHERE shop_vendor.vend_name = '?????';
-    '''
-    prods = Product.objects.all()
-    abibas__prods     = a = prods.filter(vend_id__vend_name="Abibas")
-    banana__prods     = b = prods.filter(vend_id__vend_name="Banana")
-    f4fashion__prods  = c = prods.filter(vend_id__vend_name="F 4 Fashion")
-    fink__prods       = d = prods.filter(vend_id__vend_name="Fink Manufacturing")
-    microhard__prods  = e = prods.filter(vend_id__vend_name="Microhard")
-    mike__prods       = f = prods.filter(vend_id__vend_name="Mike")
-    penguin__prods    = g = prods.filter(vend_id__vend_name="Penguin Inc")
-    programing__prods = h = prods.filter(vend_id__vend_name="Programing Press")
-    ryan__prods       = i = prods.filter(vend_id__vend_name="Ryan Industries")
-    toysrthem__prods  = j = prods.filter(vend_id__vend_name="Toys R Them")
-    wcdonalds__prods  = k = prods.filter(vend_id__vend_name="WcDonald's")
-    unknown__prods    = l = prods.filter(vend_id__vend_name="Unknown")
-    prods_group = (a, b, c, d, e, f, g, h, i, j, k, l)
-
-    context = {
-        'current_time': current_time,
-        'current_hour': current_hour,
-        'prods_group': prods_group,
-    }
-    return render(request, 'shop/product_vendor_list.html', context=context)
+    return render(request, 'shop/product_list.html', context=context)
 
 
 def product_detail_view(request, pk):
