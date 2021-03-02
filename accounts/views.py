5612c8a74377f842d458ca1b3ee10e93f55342f6
Wei-Ting Yang <williamatcg@gmail.com>
2021-03-02 17:58:49 +0800

    SignupView(View) --> SignupView(CreateView)

--- accounts/views.py
+++ accounts/views.py
@@ -1,21 +1,9 @@
-from django.views import View
+from django.views.generic.edit import CreateView
 from django.contrib.auth.forms import UserCreationForm
-from django.shortcuts import render
-from django.http import HttpResponseRedirect
-from django.urls import reverse
+from django.urls import reverse_lazy
 
 
-class SignupView(View):
+class SignupView(CreateView):
     form_class = UserCreationForm
     template_name = 'accounts/signup.html'
-
-    def get(self, request, *args, **kwargs):
-        form = self.form_class()
-        return render(request, self.template_name, context={'form': form})
-
-    def post(self, request, *args, **kwargs):
-        form = self.form_class(request.POST)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect(reverse('login'))
-        return render(request, self.template_name, context={'form': form})
+    success_url = reverse_lazy('login')
