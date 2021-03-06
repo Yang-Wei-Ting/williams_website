a5c478212b3a5e785edac4dc0bf6ba43c66af56e
Wei-Ting Yang <williamatcg@gmail.com>
2021-03-06 23:43:55 +0800

    updated home test

--- home/tests.py
+++ home/tests.py
@@ -1,14 +1,13 @@
 from django.test import TestCase
+from django.urls import reverse
 
-from .models import HomeCaption
 
-
-class HomeModelsTest(TestCase):
-    def setUp(self):
-        self.home_caption = HomeCaption.objects.create(
-            purpose="purpose",
-            text="text",
+class HomeTest(TestCase):
+    # views
+    def test_home_view(self):
+        response = self.client.get(
+            reverse('home'),
+            secure=True,
         )
-
-    def test_string_representation(self):
-        self.assertEqual(str(self.home_caption), self.home_caption.purpose)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home/home.html')
